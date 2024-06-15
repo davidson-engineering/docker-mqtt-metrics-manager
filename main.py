aa33caf4a5ac1730323159f9eb550902a5e913c2
Matthew Davidson <admin@davidson.engineering>
2024-06-15 12:28:14 -0700

    first commit

--- main.py
+++ main.py
@@ -8,10 +8,17 @@
 """a_short_project_description"""
 # ---------------------------------------------------------------------------
 
+from mqtt_node_network.configure import initialize
+from mqtt_node_network.client import MQTTClient
 
-def main():
-    pass
+config = initialize(
+    config="config/config.toml", secrets=".env", logger="config/logger.yaml"
+)
+
+
+def start_client():
+    client = MQTTClient(broker_config=config["mqtt"]["broker"])
 
 
 if __name__ == "__main__":
-    main()
+    start_client()
